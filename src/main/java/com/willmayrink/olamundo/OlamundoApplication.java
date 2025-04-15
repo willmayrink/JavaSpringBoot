409838517d2422ea28bd451e2571763fedbe0f81
Will Mayrink <will.mayrink@gmail.com>
2025-04-15 15:22:54 -0300

    first commit

---
+++ src/main/java/com/willmayrink/olamundo/OlamundoApplication.java
@@ -0,0 +1,13 @@
+package com.willmayrink.olamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlamundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willmayrink/olamundo/OlamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willmayrink.olamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
